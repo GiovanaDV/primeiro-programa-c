4ffe49b467a6b3fec08f63d189f924b8a6b4ecd0
Giovana <giovana.dvalentini@gmail.com>
2025-04-01 09:21:47 -0300

    Adicionando meu primeiro programa em C

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //inclui biblioteca padrão de entrada e saída do C
+int main()         // declaracao funcao main - o ponto de entrada
+{
+    printf("Ola, Mundo!\n");
+    return 0; // indica que o programa terminou com sucesso - valores diferentes de 0 = erro
+}
