3fa17036c5ab9896e14ce967c8e75df5e123d1da
Giovana Dias Valentini <107136020+GiovanaDV@users.noreply.github.com>
2025-04-07 14:54:21 -0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h> //inclui biblioteca padrão de entrada e saída do C
-int main()         // declaracao funcao main - o ponto de entrada
+int main()         // declaracao funcao main - o ponto de entrada (onde execucao comeca)
 {
     printf("Ola, Mundo!\n");
-    return 0; // indica que o programa terminou com sucesso - valores diferentes de 0 = erro
+    return 0; // indica que o programa terminou com sucesso 
 }
+
+// <stdio.h> contem funcoes essenciais para imprimir na tela e ler dados do usuario, etc; h significa
+// header; "Standard Input and Output Header"
+
+// int depois de main() indica que essa funcao retorna um numero inteiro (codigo de saida do programa)
+
+// printf: permite formatar o texto com especificadores de formato como %d (int), %f (float), %s (string)
+
+// se o programa retornou 0 = compilou com sucesso
+// programa retornou != 0 = podem indicar que ocorreu algum erro
